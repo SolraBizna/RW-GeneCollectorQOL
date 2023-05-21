494fbdf2b8e4452432ef16e20d0c109bd3ddc400
Andrey "Zed" Zaikin <andrey.zaikin@every-pay.com>
2023-05-21 10:03:34 +0300

    RandomsGeneAssistant fix

--- Source/Patch_RandomsGeneAssistant.cs
+++ Source/Patch_RandomsGeneAssistant.cs
@@ -10,49 +10,46 @@ using System.Reflection.Emit;
 
 namespace zed_0xff.GeneCollectorQOL;
 
+// Random's Gene Assistant: support all genepacks
 public static class Patch_RandomsGeneAssistant
 {
     const string PackageId = "rimworld.randomcoughdrop.geneassistant";
 
     public static void HandlePatch(Harmony harmony) {
         if (LoadedModManager.RunningModsListForReading.Any(m => m.PackageId == PackageId)) {
-            foreach( MethodBase m in TargetMethods() ){
-                harmony.Patch(m,
-                        transpiler: new HarmonyMethod(
-                            MethodBase.GetCurrentMethod().DeclaringType.GetMethod("Transpiler", BindingFlags.Static | BindingFlags.Public),
-                            after: new[] { PackageId }
-                            )
-                        );
-            }
+            harmony.Patch(TargetMethod(),
+                    transpiler: new HarmonyMethod(
+                        MethodBase.GetCurrentMethod().DeclaringType.GetMethod("Transpiler", BindingFlags.Static | BindingFlags.Public),
+                        after: new[] { PackageId }
+                        )
+                    );
         }
     }
 
-    public static IEnumerable<MethodBase> TargetMethods()
+    public static MethodBase TargetMethod()
     {
-        // hobtook.tradeui
-        // yield return AccessTools.FirstMethod(AccessTools.TypeByName("Harmony_DialogTrade_FillMainRect"), m => m.Name == "MyDrawTradableRow");
-        yield return AccessTools.Method(typeof(RimWorld.TradeUI), nameof(RimWorld.TradeUI.DrawTradeableRow));
+        MethodInfo mi = AccessTools.Method(AccessTools.TypeByName("RandomsGeneAssistant.PatchTraderColorChange"), "DrawTradeableRow_Prefix");
+        return mi;
     }
 
-    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator gen)
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        MethodInfo m = AccessTools.Method(typeof(Transferable), nameof(Transferable.ThingDef));
-        FieldInfo f = AccessTools.Field(typeof(ThingDefOf), nameof(ThingDefOf.Genepack));
+        MethodInfo get_ThingDef = AccessTools.PropertyGetter(typeof(Transferable), nameof(Transferable.ThingDef));
+        MethodInfo get_AnyThing = AccessTools.PropertyGetter(typeof(Transferable), nameof(Transferable.AnyThing));
+        FieldInfo fGenepack = AccessTools.Field(typeof(ThingDefOf), nameof(ThingDefOf.Genepack));
 
         var codes = new List<CodeInstruction>(instructions);
         for (int i = 0; i < codes.Count; i++){
             if (
-                    codes[i].Calls(m) &&
-                    codes[i+1].LoadsField(f) &&
+                    codes[i].Calls(get_ThingDef) &&
+                    codes[i+1].LoadsField(fGenepack) &&
                     codes[i+2].opcode == OpCodes.Beq_S &&
                     codes[i+3].opcode == OpCodes.Ret
                ) {
-                var label = gen.DefineLabel();
+                yield return new CodeInstruction(OpCodes.Callvirt, get_AnyThing);
                 yield return new CodeInstruction(OpCodes.Isinst, typeof(Genepack));
-                yield return new CodeInstruction(OpCodes.Brtrue, label);
-                yield return new CodeInstruction(OpCodes.Ret);
-                yield return new CodeInstruction(OpCodes.Nop).WithLabels(label);
-                i+=3;
+                yield return new CodeInstruction(OpCodes.Brtrue, codes[i+2].operand);
+                i+=2;
             } else {
                 yield return codes[i];
             }
